2af9861b4e49a993a29b13b0fed2f9cb7b1e2b59
VaibhavBansal05 <vaibhavkrishnabansal@gmail.com>
2024-06-13 13:55:38 +0530

    modified

--- OpenCV_project.py
+++ OpenCV_project.py
@@ -1,33 +1,40 @@
-import cv2 as cv
 import numpy as np
-import sys
+import cv2
+from PIL import Image 
 
-video = cv.VideoCapture(0)
+#from util import get_limits
 
-if not video.isOpened():
-    sys.exit("unable to detect camera")
 
+yellow = [0, 255, 255]  # yellow in BGR colorspace
+cap = cv2.VideoCapture(0)
 while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_yellow = np.array([0, 170, 70])
+    upper_yellow = np.array([9, 255, 255])
+
+    mask = cv2.inRange(hsvImage, lower_yellow, upper_yellow)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    kernel = np.ones((3,3),np.uint8)
+    erosion = cv2.erode(res,kernel,iterations = 1)
+
+    mask_ = Image.fromarray(erosion)
     
-    __, frame = video.read()
-    
-    cvt = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    
-    edge = cv.goodFeaturesToTrack(cvt, 1000, 0.01, 10)
-    
-    coners = np.int32(edge)
-    
-    for lines in coners:
-        x,y = lines.ravel()
-        
-        cv.circle(frame, (x,y), 4, (0,0,255), -1)
-        
-    cv.imshow("img", frame)
-    
-    key = cv.waitKey(1)
-    
-    if key == ord("q"):
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    cv2.imshow('frame', frame)
+    #cv2.imshow('erode', erosion)
+    #cv2.imshow('res', res)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cv.destroyAllWindows()
-video.release()
+cap.release()
+
+cv2.destroyAllWindows()
